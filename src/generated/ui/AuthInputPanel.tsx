ae04b22700df15e1f40261153030826c2f03a196
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:42:30 +0530

    feat(ui): create new AuthInputPanel module

--- src/generated/ui/AuthInputPanel.tsx
+++ src/generated/ui/AuthInputPanel.tsx
@@ -5,20 +5,6 @@ interface AuthInputPanelProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_704?: string;
-  prop_341?: string;
-  prop_731?: string;
-  prop_464?: string;
-  prop_453?: string;
-  prop_596?: string;
-  prop_790?: string;
-  prop_357?: string;
-  prop_784?: string;
-  prop_629?: string;
-  prop_286?: string;
-  prop_822?: string;
-  prop_179?: string;
-  prop_765?: string;
   // PROPS_PLACEHOLDER
 }
 
