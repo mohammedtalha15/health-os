b4e87941455cdf020bdb46f47b127b4fe6573f9e
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:41:27 +0530

    feat(ui): create new AdminToggleSidebar module

--- src/generated/ui/AdminToggleSidebar.tsx
+++ src/generated/ui/AdminToggleSidebar.tsx
@@ -5,15 +5,6 @@ interface AdminToggleSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_972?: string;
-  prop_951?: string;
-  prop_993?: string;
-  prop_360?: string;
-  prop_355?: string;
-  prop_733?: string;
-  prop_323?: string;
-  prop_429?: string;
-  prop_654?: string;
   // PROPS_PLACEHOLDER
 }
 
