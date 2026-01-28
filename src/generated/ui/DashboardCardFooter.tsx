c635d997c56b54d88fbac091bda690e4790666f8
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:33:45 +0530

    feat(ui): create new DashboardCardFooter module

--- src/generated/ui/DashboardCardFooter.tsx
+++ src/generated/ui/DashboardCardFooter.tsx
@@ -5,19 +5,6 @@ interface DashboardCardFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_336?: string;
-  prop_880?: string;
-  prop_794?: string;
-  prop_112?: string;
-  prop_201?: string;
-  prop_731?: string;
-  prop_853?: string;
-  prop_215?: string;
-  prop_518?: string;
-  prop_551?: string;
-  prop_488?: string;
-  prop_392?: string;
-  prop_180?: string;
   // PROPS_PLACEHOLDER
 }
 
