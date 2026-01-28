1580eee270922e2d8ba20ea33cbad7daf2dbf293
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:43:08 +0530

    feat(ui): create new ModalDatabox module

--- src/generated/ui/ModalDatabox.tsx
+++ src/generated/ui/ModalDatabox.tsx
@@ -5,34 +5,6 @@ interface ModalDataboxProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_749?: string;
-  prop_809?: string;
-  prop_698?: string;
-  prop_735?: string;
-  prop_440?: string;
-  prop_433?: string;
-  prop_472?: string;
-  prop_142?: string;
-  prop_114?: string;
-  prop_938?: string;
-  prop_992?: string;
-  prop_377?: string;
-  prop_907?: string;
-  prop_819?: string;
-  prop_230?: string;
-  prop_249?: string;
-  prop_786?: string;
-  prop_960?: string;
-  prop_462?: string;
-  prop_386?: string;
-  prop_288?: string;
-  prop_897?: string;
-  prop_384?: string;
-  prop_829?: string;
-  prop_446?: string;
-  prop_776?: string;
-  prop_207?: string;
-  prop_150?: string;
   // PROPS_PLACEHOLDER
 }
 
