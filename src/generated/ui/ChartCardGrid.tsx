e878022fa11a4fb1131314313c7a8e1b396591d7
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:42:23 +0530

    feat(ui): create new ChartCardGrid module

--- src/generated/ui/ChartCardGrid.tsx
+++ src/generated/ui/ChartCardGrid.tsx
@@ -5,9 +5,6 @@ interface ChartCardGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_603?: string;
-  prop_105?: string;
-  prop_805?: string;
   // PROPS_PLACEHOLDER
 }
 
