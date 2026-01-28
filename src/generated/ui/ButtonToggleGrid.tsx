264f7011a1fa42d855135cc3114aa6a8bf870a78
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:50:16 +0530

    feat(ui): create new ButtonToggleGrid module

--- src/generated/ui/ButtonToggleGrid.tsx
+++ src/generated/ui/ButtonToggleGrid.tsx
@@ -5,9 +5,6 @@ interface ButtonToggleGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_790?: string;
-  prop_429?: string;
-  prop_229?: string;
   // PROPS_PLACEHOLDER
 }
 
