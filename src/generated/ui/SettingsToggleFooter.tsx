1cf48e4568b4fb31bd2161b39a9378053836ff2f
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:48:56 +0530

    feat(ui): create new SettingsToggleFooter module

--- src/generated/ui/SettingsToggleFooter.tsx
+++ src/generated/ui/SettingsToggleFooter.tsx
@@ -5,9 +5,6 @@ interface SettingsToggleFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_163?: string;
-  prop_339?: string;
-  prop_407?: string;
   // PROPS_PLACEHOLDER
 }
 
