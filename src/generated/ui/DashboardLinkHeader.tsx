2dc8da0b8486781fd1bb1c7d0c9893aeb4e428a8
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:11:46 +0530

    feat(ui): create new DashboardLinkHeader module

--- src/generated/ui/DashboardLinkHeader.tsx
+++ src/generated/ui/DashboardLinkHeader.tsx
@@ -5,9 +5,6 @@ interface DashboardLinkHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_302?: string;
-  prop_730?: string;
-  prop_455?: string;
   // PROPS_PLACEHOLDER
 }
 
