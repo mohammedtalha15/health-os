26aa8a9c2803f3e17d37a5b141fad7bf8601cab7
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:54:16 +0530

    feat(ui): create new AuthLinkGroup module

--- src/generated/ui/AuthLinkGroup.tsx
+++ src/generated/ui/AuthLinkGroup.tsx
@@ -5,42 +5,6 @@ interface AuthLinkGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_629?: string;
-  prop_570?: string;
-  prop_711?: string;
-  prop_245?: string;
-  prop_551?: string;
-  prop_816?: string;
-  prop_456?: string;
-  prop_685?: string;
-  prop_962?: string;
-  prop_594?: string;
-  prop_653?: string;
-  prop_725?: string;
-  prop_214?: string;
-  prop_964?: string;
-  prop_487?: string;
-  prop_751?: string;
-  prop_341?: string;
-  prop_181?: string;
-  prop_384?: string;
-  prop_332?: string;
-  prop_540?: string;
-  prop_321?: string;
-  prop_419?: string;
-  prop_619?: string;
-  prop_111?: string;
-  prop_447?: string;
-  prop_664?: string;
-  prop_292?: string;
-  prop_930?: string;
-  prop_568?: string;
-  prop_798?: string;
-  prop_320?: string;
-  prop_540?: string;
-  prop_363?: string;
-  prop_723?: string;
-  prop_339?: string;
   // PROPS_PLACEHOLDER
 }
 
