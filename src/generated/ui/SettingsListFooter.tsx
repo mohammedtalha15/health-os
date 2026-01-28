e2583be6b9f19036726bae6f71dda1ef0976f7f2
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:25:18 +0530

    feat(ui): create new SettingsListFooter module

--- src/generated/ui/SettingsListFooter.tsx
+++ src/generated/ui/SettingsListFooter.tsx
@@ -5,9 +5,6 @@ interface SettingsListFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_972?: string;
-  prop_934?: string;
-  prop_568?: string;
   // PROPS_PLACEHOLDER
 }
 
