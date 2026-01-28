89829d49050ab6c2bb02bd2a7feb3094e2eab0a7
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:19:28 +0530

    feat(ui): create new AuthDataGroup module

--- src/generated/ui/AuthDataGroup.tsx
+++ src/generated/ui/AuthDataGroup.tsx
@@ -5,9 +5,6 @@ interface AuthDataGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_770?: string;
-  prop_193?: string;
-  prop_196?: string;
   // PROPS_PLACEHOLDER
 }
 
