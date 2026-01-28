d9a432eadebf90753dd1bb6df35ddc15c2054f4f
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:07:11 +0530

    feat(ui): create new DashboardToggleRow module

--- src/generated/ui/DashboardToggleRow.tsx
+++ src/generated/ui/DashboardToggleRow.tsx
@@ -5,9 +5,6 @@ interface DashboardToggleRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_250?: string;
-  prop_163?: string;
-  prop_931?: string;
   // PROPS_PLACEHOLDER
 }
 
