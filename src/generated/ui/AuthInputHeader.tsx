153fd26ccbe1941508e17a4f959661d16020da8e
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:49:13 +0530

    feat(ui): create new AuthInputHeader module

--- src/generated/ui/AuthInputHeader.tsx
+++ src/generated/ui/AuthInputHeader.tsx
@@ -5,12 +5,6 @@ interface AuthInputHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_429?: string;
-  prop_252?: string;
-  prop_607?: string;
-  prop_533?: string;
-  prop_594?: string;
-  prop_887?: string;
   // PROPS_PLACEHOLDER
 }
 
