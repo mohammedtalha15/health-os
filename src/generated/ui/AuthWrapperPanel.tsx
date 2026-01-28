9182898946bfe001b36541c822609040d32cadaa
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:30:52 +0530

    feat(ui): create new AuthWrapperPanel module

--- src/generated/ui/AuthWrapperPanel.tsx
+++ src/generated/ui/AuthWrapperPanel.tsx
@@ -5,9 +5,6 @@ interface AuthWrapperPanelProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_208?: string;
-  prop_259?: string;
-  prop_566?: string;
   // PROPS_PLACEHOLDER
 }
 
