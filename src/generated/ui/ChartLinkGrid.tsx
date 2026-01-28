a0f8d5e24f1945c52534bc4e6029d51564cc8e6c
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:41:06 +0530

    feat(ui): create new ChartLinkGrid module

--- src/generated/ui/ChartLinkGrid.tsx
+++ src/generated/ui/ChartLinkGrid.tsx
@@ -5,9 +5,6 @@ interface ChartLinkGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_961?: string;
-  prop_182?: string;
-  prop_208?: string;
   // PROPS_PLACEHOLDER
 }
 
