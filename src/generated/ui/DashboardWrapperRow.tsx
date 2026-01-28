7e9234599dbbfe2c7caed4c8e72ec9cd5dcc08ed
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:58:44 +0530

    feat(ui): create new DashboardWrapperRow module

--- src/generated/ui/DashboardWrapperRow.tsx
+++ src/generated/ui/DashboardWrapperRow.tsx
@@ -5,15 +5,6 @@ interface DashboardWrapperRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_509?: string;
-  prop_415?: string;
-  prop_247?: string;
-  prop_769?: string;
-  prop_482?: string;
-  prop_905?: string;
-  prop_439?: string;
-  prop_291?: string;
-  prop_633?: string;
   // PROPS_PLACEHOLDER
 }
 
