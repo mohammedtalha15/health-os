4f7514488c1468a5b409d611ffd07b656006c475
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:37:12 +0530

    feat(ui): create new TableDataSidebar module

--- src/generated/ui/TableDataSidebar.tsx
+++ src/generated/ui/TableDataSidebar.tsx
@@ -5,9 +5,6 @@ interface TableDataSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_829?: string;
-  prop_323?: string;
-  prop_291?: string;
   // PROPS_PLACEHOLDER
 }
 
