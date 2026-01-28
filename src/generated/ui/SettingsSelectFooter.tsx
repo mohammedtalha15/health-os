8ccfa1ce8b47e9434ba8f7614e63e8f5648d6926
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:17:18 +0530

    feat(ui): create new SettingsSelectFooter module

--- src/generated/ui/SettingsSelectFooter.tsx
+++ src/generated/ui/SettingsSelectFooter.tsx
@@ -5,11 +5,6 @@ interface SettingsSelectFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_142?: string;
-  prop_612?: string;
-  prop_456?: string;
-  prop_873?: string;
-  prop_198?: string;
   // PROPS_PLACEHOLDER
 }
 
