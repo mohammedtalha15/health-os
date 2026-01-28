366b2b6f14c970e99ee61e1a08af4a64cfd2d7fd
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:27:07 +0530

    feat(ui): create new DashboardSelectGroup module

--- src/generated/ui/DashboardSelectGroup.tsx
+++ src/generated/ui/DashboardSelectGroup.tsx
@@ -5,24 +5,6 @@ interface DashboardSelectGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_992?: string;
-  prop_927?: string;
-  prop_859?: string;
-  prop_493?: string;
-  prop_476?: string;
-  prop_370?: string;
-  prop_876?: string;
-  prop_518?: string;
-  prop_778?: string;
-  prop_404?: string;
-  prop_343?: string;
-  prop_979?: string;
-  prop_949?: string;
-  prop_302?: string;
-  prop_639?: string;
-  prop_307?: string;
-  prop_740?: string;
-  prop_628?: string;
   // PROPS_PLACEHOLDER
 }
 
