2504f9444d3a4a2d1c806a6146d71dceaae2b28d
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:43:47 +0530

    feat(ui): create new ProfileWrapperHeader module

--- src/generated/ui/ProfileWrapperHeader.tsx
+++ src/generated/ui/ProfileWrapperHeader.tsx
@@ -5,11 +5,6 @@ interface ProfileWrapperHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_572?: string;
-  prop_677?: string;
-  prop_266?: string;
-  prop_197?: string;
-  prop_574?: string;
   // PROPS_PLACEHOLDER
 }
 
