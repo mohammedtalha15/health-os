25453e033c8cc34018909c9c98a00d4da6ce0736
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:26:41 +0530

    feat(ui): create new ModalContainerbox module

--- src/generated/ui/ModalContainerbox.tsx
+++ src/generated/ui/ModalContainerbox.tsx
@@ -5,9 +5,6 @@ interface ModalContainerboxProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_376?: string;
-  prop_163?: string;
-  prop_546?: string;
   // PROPS_PLACEHOLDER
 }
 
