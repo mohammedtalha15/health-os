069c50369657f21470055060dcb8d81c76b81906
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:15:54 +0530

    feat(ui): create new DashboardContainerSidebar module

--- src/generated/ui/DashboardContainerSidebar.tsx
+++ src/generated/ui/DashboardContainerSidebar.tsx
@@ -5,9 +5,6 @@ interface DashboardContainerSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_710?: string;
-  prop_775?: string;
-  prop_443?: string;
   // PROPS_PLACEHOLDER
 }
 
