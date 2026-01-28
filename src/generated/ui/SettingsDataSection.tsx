0f332e14db5db93a8ab9c10906ffbe731770ec18
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:49:10 +0530

    feat(ui): create new SettingsDataSection module

--- src/generated/ui/SettingsDataSection.tsx
+++ src/generated/ui/SettingsDataSection.tsx
@@ -5,9 +5,6 @@ interface SettingsDataSectionProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_913?: string;
-  prop_328?: string;
-  prop_542?: string;
   // PROPS_PLACEHOLDER
 }
 
