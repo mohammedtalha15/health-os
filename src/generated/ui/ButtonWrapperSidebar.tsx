ca56913d6347a0de3815ae96600da56c03f8a865
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:01:37 +0530

    feat(ui): create new ButtonWrapperSidebar module

--- src/generated/ui/ButtonWrapperSidebar.tsx
+++ src/generated/ui/ButtonWrapperSidebar.tsx
@@ -5,10 +5,6 @@ interface ButtonWrapperSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_935?: string;
-  prop_121?: string;
-  prop_178?: string;
-  prop_547?: string;
   // PROPS_PLACEHOLDER
 }
 
