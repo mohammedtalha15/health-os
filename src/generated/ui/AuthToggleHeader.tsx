851d13d3c663ac4ac546e49c320fcbaee8f014e2
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:24:29 +0530

    feat(ui): create new AuthToggleHeader module

--- src/generated/ui/AuthToggleHeader.tsx
+++ src/generated/ui/AuthToggleHeader.tsx
@@ -5,9 +5,6 @@ interface AuthToggleHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_574?: string;
-  prop_279?: string;
-  prop_969?: string;
   // PROPS_PLACEHOLDER
 }
 
