edf1965f0bd07369472137f219a38cbd8ed4f689
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:02:23 +0530

    feat(ui): create new TableContainerGrid module

--- src/generated/ui/TableContainerGrid.tsx
+++ src/generated/ui/TableContainerGrid.tsx
@@ -5,25 +5,6 @@ interface TableContainerGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_454?: string;
-  prop_867?: string;
-  prop_441?: string;
-  prop_953?: string;
-  prop_129?: string;
-  prop_569?: string;
-  prop_178?: string;
-  prop_294?: string;
-  prop_288?: string;
-  prop_664?: string;
-  prop_617?: string;
-  prop_156?: string;
-  prop_120?: string;
-  prop_540?: string;
-  prop_707?: string;
-  prop_422?: string;
-  prop_664?: string;
-  prop_998?: string;
-  prop_772?: string;
   // PROPS_PLACEHOLDER
 }
 
