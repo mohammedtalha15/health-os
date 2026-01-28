8f992819c16141a4f65a88d04978ae41b83bb721
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:03:52 +0530

    feat(ui): create new ButtonSelectGroup module

--- src/generated/ui/ButtonSelectGroup.tsx
+++ src/generated/ui/ButtonSelectGroup.tsx
@@ -5,10 +5,6 @@ interface ButtonSelectGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_698?: string;
-  prop_594?: string;
-  prop_362?: string;
-  prop_144?: string;
   // PROPS_PLACEHOLDER
 }
 
