9e9a07223e6c3021d66ef0d02c8c570d0d7388c2
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:05:06 +0530

    feat(ui): create new AuthListRow module

--- src/generated/ui/AuthListRow.tsx
+++ src/generated/ui/AuthListRow.tsx
@@ -5,10 +5,6 @@ interface AuthListRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_375?: string;
-  prop_208?: string;
-  prop_713?: string;
-  prop_292?: string;
   // PROPS_PLACEHOLDER
 }
 
