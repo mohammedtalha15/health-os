2e2bad617effb962f662944ac6fb45d6966bd94b
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:06:04 +0530

    feat(ui): create new ChartViewbox module

--- src/generated/ui/ChartViewbox.tsx
+++ src/generated/ui/ChartViewbox.tsx
@@ -5,30 +5,6 @@ interface ChartViewboxProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_319?: string;
-  prop_771?: string;
-  prop_891?: string;
-  prop_641?: string;
-  prop_207?: string;
-  prop_634?: string;
-  prop_991?: string;
-  prop_261?: string;
-  prop_959?: string;
-  prop_272?: string;
-  prop_576?: string;
-  prop_709?: string;
-  prop_400?: string;
-  prop_540?: string;
-  prop_225?: string;
-  prop_986?: string;
-  prop_468?: string;
-  prop_359?: string;
-  prop_342?: string;
-  prop_504?: string;
-  prop_308?: string;
-  prop_149?: string;
-  prop_891?: string;
-  prop_150?: string;
   // PROPS_PLACEHOLDER
 }
 
