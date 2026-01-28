0e17ce22a9339f0fa1faeeb49bdd0d6d495266ad
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:07:43 +0530

    feat(ui): create new AuthCardSidebar module

--- src/generated/ui/AuthCardSidebar.tsx
+++ src/generated/ui/AuthCardSidebar.tsx
@@ -5,9 +5,6 @@ interface AuthCardSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_510?: string;
-  prop_159?: string;
-  prop_931?: string;
   // PROPS_PLACEHOLDER
 }
 
