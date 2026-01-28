11fc76174e2de724a3930e8ff0bd61e98c3d964b
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:56:52 +0530

    feat(ui): create new ButtonCardGroup module

--- src/generated/ui/ButtonCardGroup.tsx
+++ src/generated/ui/ButtonCardGroup.tsx
@@ -5,23 +5,6 @@ interface ButtonCardGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_349?: string;
-  prop_775?: string;
-  prop_595?: string;
-  prop_350?: string;
-  prop_350?: string;
-  prop_947?: string;
-  prop_538?: string;
-  prop_936?: string;
-  prop_321?: string;
-  prop_742?: string;
-  prop_610?: string;
-  prop_170?: string;
-  prop_511?: string;
-  prop_287?: string;
-  prop_190?: string;
-  prop_837?: string;
-  prop_289?: string;
   // PROPS_PLACEHOLDER
 }
 
