d57e8b82db29e9b21eb7e49475bf91d68fdca11a
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:06:01 +0530

    feat(ui): create new ModalSelectSection module

--- src/generated/ui/ModalSelectSection.tsx
+++ src/generated/ui/ModalSelectSection.tsx
@@ -5,9 +5,6 @@ interface ModalSelectSectionProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_731?: string;
-  prop_763?: string;
-  prop_239?: string;
   // PROPS_PLACEHOLDER
 }
 
