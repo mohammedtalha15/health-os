d0f699d8967b5f32398e0140c1123db74528b3ca
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:47:28 +0530

    feat(ui): create new ProfileLinkHeader module

--- src/generated/ui/ProfileLinkHeader.tsx
+++ src/generated/ui/ProfileLinkHeader.tsx
@@ -5,9 +5,6 @@ interface ProfileLinkHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_269?: string;
-  prop_922?: string;
-  prop_340?: string;
   // PROPS_PLACEHOLDER
 }
 
