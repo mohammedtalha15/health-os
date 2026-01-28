b8fad14f25accbc0185794a6639a7c02745e1ac8
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:15:29 +0530

    feat(ui): create new AdminSelectGrid module

--- src/generated/ui/AdminSelectGrid.tsx
+++ src/generated/ui/AdminSelectGrid.tsx
@@ -5,27 +5,6 @@ interface AdminSelectGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_132?: string;
-  prop_982?: string;
-  prop_380?: string;
-  prop_518?: string;
-  prop_916?: string;
-  prop_829?: string;
-  prop_847?: string;
-  prop_738?: string;
-  prop_884?: string;
-  prop_168?: string;
-  prop_951?: string;
-  prop_648?: string;
-  prop_615?: string;
-  prop_863?: string;
-  prop_371?: string;
-  prop_544?: string;
-  prop_770?: string;
-  prop_213?: string;
-  prop_645?: string;
-  prop_331?: string;
-  prop_985?: string;
   // PROPS_PLACEHOLDER
 }
 
