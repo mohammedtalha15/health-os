b948b9a33323a8979c7e7dba0d4e9a2c28bccff8
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:36:24 +0530

    feat(ui): create new DashboardCardbox module

--- src/generated/ui/DashboardCardbox.tsx
+++ src/generated/ui/DashboardCardbox.tsx
@@ -5,9 +5,6 @@ interface DashboardCardboxProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_594?: string;
-  prop_297?: string;
-  prop_712?: string;
   // PROPS_PLACEHOLDER
 }
 
