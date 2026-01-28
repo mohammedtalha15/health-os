7f89f96318b645340a1b33eb7a4b4b846d4f4fdb
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:44:15 +0530

    feat(ui): create new UserSelectHeader module

--- src/generated/ui/UserSelectHeader.tsx
+++ src/generated/ui/UserSelectHeader.tsx
@@ -5,9 +5,6 @@ interface UserSelectHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_372?: string;
-  prop_531?: string;
-  prop_822?: string;
   // PROPS_PLACEHOLDER
 }
 
