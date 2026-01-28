069ced9aee02885aad7826d8495b45ca6d073982
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:14:30 +0530

    feat(ui): create new AuthCardGrid module

--- src/generated/ui/AuthCardGrid.tsx
+++ src/generated/ui/AuthCardGrid.tsx
@@ -5,9 +5,6 @@ interface AuthCardGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_314?: string;
-  prop_505?: string;
-  prop_473?: string;
   // PROPS_PLACEHOLDER
 }
 
