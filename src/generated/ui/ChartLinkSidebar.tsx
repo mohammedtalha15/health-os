5c0d083a21a055a93c631b87cbabf4559f7301dc
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:11:29 +0530

    feat(ui): create new ChartLinkSidebar module

--- src/generated/ui/ChartLinkSidebar.tsx
+++ src/generated/ui/ChartLinkSidebar.tsx
@@ -5,9 +5,6 @@ interface ChartLinkSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_238?: string;
-  prop_204?: string;
-  prop_436?: string;
   // PROPS_PLACEHOLDER
 }
 
