794a56e7b2216078da174c3c5a178828f51fee8e
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:49:08 +0530

    feat(ui): create new AuthSelectbox module

--- src/generated/ui/AuthSelectbox.tsx
+++ src/generated/ui/AuthSelectbox.tsx
@@ -5,21 +5,6 @@ interface AuthSelectboxProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_388?: string;
-  prop_578?: string;
-  prop_919?: string;
-  prop_805?: string;
-  prop_516?: string;
-  prop_752?: string;
-  prop_496?: string;
-  prop_596?: string;
-  prop_858?: string;
-  prop_342?: string;
-  prop_611?: string;
-  prop_739?: string;
-  prop_743?: string;
-  prop_803?: string;
-  prop_635?: string;
   // PROPS_PLACEHOLDER
 }
 
