c6273ca07fc6dfcc8f23262f1f4340f92a525b5f
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:46:41 +0530

    feat(ui): create new SettingsSelectItem module

--- src/generated/ui/SettingsSelectItem.tsx
+++ src/generated/ui/SettingsSelectItem.tsx
@@ -5,9 +5,6 @@ interface SettingsSelectItemProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_185?: string;
-  prop_561?: string;
-  prop_949?: string;
   // PROPS_PLACEHOLDER
 }
 
