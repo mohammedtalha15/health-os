9a44b64ab245279f2ab58ca42759f55535ce1602
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:43:01 +0530

    feat(ui): create new TableViewRow module

--- src/generated/ui/TableViewRow.tsx
+++ src/generated/ui/TableViewRow.tsx
@@ -5,9 +5,6 @@ interface TableViewRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_657?: string;
-  prop_880?: string;
-  prop_247?: string;
   // PROPS_PLACEHOLDER
 }
 
