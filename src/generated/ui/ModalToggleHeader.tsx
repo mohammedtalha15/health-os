d3cba8c4e715f958288bf8222a1e965760ab17a6
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 00:58:12 +0530

    feat(ui): create new ModalToggleHeader module

--- src/generated/ui/ModalToggleHeader.tsx
+++ src/generated/ui/ModalToggleHeader.tsx
@@ -5,11 +5,6 @@ interface ModalToggleHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_813?: string;
-  prop_621?: string;
-  prop_651?: string;
-  prop_755?: string;
-  prop_426?: string;
   // PROPS_PLACEHOLDER
 }
 
