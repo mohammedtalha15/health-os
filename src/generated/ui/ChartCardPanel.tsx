336d9e3c1f96ba9eacd985afff81ff8d9e521d07
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:22:42 +0530

    feat(ui): create new ChartCardPanel module

--- src/generated/ui/ChartCardPanel.tsx
+++ src/generated/ui/ChartCardPanel.tsx
@@ -5,9 +5,6 @@ interface ChartCardPanelProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_317?: string;
-  prop_908?: string;
-  prop_930?: string;
   // PROPS_PLACEHOLDER
 }
 
