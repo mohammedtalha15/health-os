2ec3378fe8a4b559ea5ee8113cf35e95f18e4e0e
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:05:31 +0530

    feat(ui): create new AdminViewItem module

--- src/generated/ui/AdminViewItem.tsx
+++ src/generated/ui/AdminViewItem.tsx
@@ -5,11 +5,6 @@ interface AdminViewItemProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_693?: string;
-  prop_287?: string;
-  prop_618?: string;
-  prop_464?: string;
-  prop_945?: string;
   // PROPS_PLACEHOLDER
 }
 
