e173c5cc2d4c2c28958403a92c3d1605e5079cf4
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:09:41 +0530

    feat(ui): create new SettingsListRow module

--- src/generated/ui/SettingsListRow.tsx
+++ src/generated/ui/SettingsListRow.tsx
@@ -5,9 +5,6 @@ interface SettingsListRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_847?: string;
-  prop_757?: string;
-  prop_978?: string;
   // PROPS_PLACEHOLDER
 }
 
