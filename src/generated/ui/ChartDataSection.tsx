6bc8b7964cd047715c7bf6a739ce471a1a09b16c
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:26:39 +0530

    feat(ui): create new ChartDataSection module

--- src/generated/ui/ChartDataSection.tsx
+++ src/generated/ui/ChartDataSection.tsx
@@ -5,9 +5,6 @@ interface ChartDataSectionProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_797?: string;
-  prop_855?: string;
-  prop_544?: string;
   // PROPS_PLACEHOLDER
 }
 
