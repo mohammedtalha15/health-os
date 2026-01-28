3a2a1843eab4c26936acfe81d2a93523f1410f2f
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:07:47 +0530

    feat(ui): create new ProfileDataSidebar module

--- src/generated/ui/ProfileDataSidebar.tsx
+++ src/generated/ui/ProfileDataSidebar.tsx
@@ -5,9 +5,6 @@ interface ProfileDataSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_888?: string;
-  prop_908?: string;
-  prop_902?: string;
   // PROPS_PLACEHOLDER
 }
 
