4d8e049b9efaf6f759bd329f6d2a91f9dbd3f5c1
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:09:53 +0530

    feat(ui): create new DashboardContainerHeader module

--- src/generated/ui/DashboardContainerHeader.tsx
+++ src/generated/ui/DashboardContainerHeader.tsx
@@ -5,10 +5,6 @@ interface DashboardContainerHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_858?: string;
-  prop_271?: string;
-  prop_198?: string;
-  prop_834?: string;
   // PROPS_PLACEHOLDER
 }
 
