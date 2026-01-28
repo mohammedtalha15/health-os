9f43ba60a345421b37b4aa468124535ed4bbeb54
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:05:34 +0530

    feat(ui): create new TableWrapperSection module

--- src/generated/ui/TableWrapperSection.tsx
+++ src/generated/ui/TableWrapperSection.tsx
@@ -5,9 +5,6 @@ interface TableWrapperSectionProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_920?: string;
-  prop_541?: string;
-  prop_469?: string;
   // PROPS_PLACEHOLDER
 }
 
