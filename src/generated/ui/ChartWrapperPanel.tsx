3aa8038fb126c2488ce2c39eaa785a18a7bf5878
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:09:42 +0530

    feat(ui): create new ChartWrapperPanel module

--- src/generated/ui/ChartWrapperPanel.tsx
+++ src/generated/ui/ChartWrapperPanel.tsx
@@ -5,9 +5,6 @@ interface ChartWrapperPanelProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_253?: string;
-  prop_950?: string;
-  prop_884?: string;
   // PROPS_PLACEHOLDER
 }
 
