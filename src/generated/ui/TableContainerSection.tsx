787e53d7c63a9a9d548c8be70255bee2b4dc30dc
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:30:42 +0530

    feat(ui): create new TableContainerSection module

--- src/generated/ui/TableContainerSection.tsx
+++ src/generated/ui/TableContainerSection.tsx
@@ -5,9 +5,6 @@ interface TableContainerSectionProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_701?: string;
-  prop_548?: string;
-  prop_839?: string;
   // PROPS_PLACEHOLDER
 }
 
