9ca7e35b709ae1de9b5e8f224dd0c14d17c04743
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:42:25 +0530

    feat(ui): create new UserListHeader module

--- src/generated/ui/UserListHeader.tsx
+++ src/generated/ui/UserListHeader.tsx
@@ -5,9 +5,6 @@ interface UserListHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_247?: string;
-  prop_470?: string;
-  prop_445?: string;
   // PROPS_PLACEHOLDER
 }
 
