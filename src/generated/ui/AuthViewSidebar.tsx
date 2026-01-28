965c465c0a5cca4cf8967fafc9389ccea0bcdff6
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:28:01 +0530

    feat(ui): create new AuthViewSidebar module

--- src/generated/ui/AuthViewSidebar.tsx
+++ src/generated/ui/AuthViewSidebar.tsx
@@ -5,9 +5,6 @@ interface AuthViewSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_649?: string;
-  prop_846?: string;
-  prop_253?: string;
   // PROPS_PLACEHOLDER
 }
 
