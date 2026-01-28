e3919982be2064e920fad48cb6082628b4f119dd
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:33:31 +0530

    feat(ui): create new UserToggleRow module

--- src/generated/ui/UserToggleRow.tsx
+++ src/generated/ui/UserToggleRow.tsx
@@ -5,9 +5,6 @@ interface UserToggleRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_713?: string;
-  prop_433?: string;
-  prop_754?: string;
   // PROPS_PLACEHOLDER
 }
 
