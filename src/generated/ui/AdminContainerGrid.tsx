5db1ff3f62d7c444c02a589261d544b1e95ff14c
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:22:47 +0530

    feat(ui): create new AdminContainerGrid module

--- src/generated/ui/AdminContainerGrid.tsx
+++ src/generated/ui/AdminContainerGrid.tsx
@@ -5,9 +5,6 @@ interface AdminContainerGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_545?: string;
-  prop_821?: string;
-  prop_828?: string;
   // PROPS_PLACEHOLDER
 }
 
