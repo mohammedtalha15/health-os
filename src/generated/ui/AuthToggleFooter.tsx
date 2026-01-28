a1f6c43ec327b3d0a573caecef6582d7bac7766e
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 00:59:38 +0530

    feat(ui): create new AuthToggleFooter module

--- src/generated/ui/AuthToggleFooter.tsx
+++ src/generated/ui/AuthToggleFooter.tsx
@@ -5,9 +5,6 @@ interface AuthToggleFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_360?: string;
-  prop_976?: string;
-  prop_678?: string;
   // PROPS_PLACEHOLDER
 }
 
