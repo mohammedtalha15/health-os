52bb549d5f57ca29a4cd46587a1f229933a28995
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:55:45 +0530

    feat(ui): create new DashboardContainerRow module

--- src/generated/ui/DashboardContainerRow.tsx
+++ src/generated/ui/DashboardContainerRow.tsx
@@ -5,9 +5,6 @@ interface DashboardContainerRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_182?: string;
-  prop_607?: string;
-  prop_645?: string;
   // PROPS_PLACEHOLDER
 }
 
