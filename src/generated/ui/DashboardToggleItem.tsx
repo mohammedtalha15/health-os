4ef636a9d086fbd06ebed425596b6a1edffdef2c
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:02:15 +0530

    feat(ui): create new DashboardToggleItem module

--- src/generated/ui/DashboardToggleItem.tsx
+++ src/generated/ui/DashboardToggleItem.tsx
@@ -5,10 +5,6 @@ interface DashboardToggleItemProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_135?: string;
-  prop_851?: string;
-  prop_547?: string;
-  prop_824?: string;
   // PROPS_PLACEHOLDER
 }
 
