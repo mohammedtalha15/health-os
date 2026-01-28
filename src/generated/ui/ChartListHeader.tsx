ba5ec979a78fc8a7fd4a6f8c8e3b132cb5484eb0
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:46:19 +0530

    feat(ui): create new ChartListHeader module

--- src/generated/ui/ChartListHeader.tsx
+++ src/generated/ui/ChartListHeader.tsx
@@ -5,11 +5,6 @@ interface ChartListHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_200?: string;
-  prop_106?: string;
-  prop_472?: string;
-  prop_640?: string;
-  prop_372?: string;
   // PROPS_PLACEHOLDER
 }
 
