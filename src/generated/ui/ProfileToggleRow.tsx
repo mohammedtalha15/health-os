1d398b130281388886ecf758b25c007d987ad8ea
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:01:20 +0530

    feat(ui): create new ProfileToggleRow module

--- src/generated/ui/ProfileToggleRow.tsx
+++ src/generated/ui/ProfileToggleRow.tsx
@@ -5,9 +5,6 @@ interface ProfileToggleRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_831?: string;
-  prop_436?: string;
-  prop_803?: string;
   // PROPS_PLACEHOLDER
 }
 
