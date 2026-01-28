c62adf367296b6ec268f680d1fd57de0c25af311
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:03:25 +0530

    feat(ui): create new ModalLinkbox module

--- src/generated/ui/ModalLinkbox.tsx
+++ src/generated/ui/ModalLinkbox.tsx
@@ -5,9 +5,6 @@ interface ModalLinkboxProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_539?: string;
-  prop_901?: string;
-  prop_315?: string;
   // PROPS_PLACEHOLDER
 }
 
