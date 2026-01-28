b1bd25dad94b61db72d19885916ba45d2d225c16
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:41:07 +0530

    feat(ui): create new DashboardListRow module

--- src/generated/ui/DashboardListRow.tsx
+++ src/generated/ui/DashboardListRow.tsx
@@ -5,11 +5,6 @@ interface DashboardListRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_118?: string;
-  prop_432?: string;
-  prop_915?: string;
-  prop_763?: string;
-  prop_684?: string;
   // PROPS_PLACEHOLDER
 }
 
