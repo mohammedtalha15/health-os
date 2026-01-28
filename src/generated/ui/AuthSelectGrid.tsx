1cc4d488995e1d596695425215eb4b118db97973
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:19:06 +0530

    feat(ui): create new AuthSelectGrid module

--- src/generated/ui/AuthSelectGrid.tsx
+++ src/generated/ui/AuthSelectGrid.tsx
@@ -5,10 +5,6 @@ interface AuthSelectGridProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_898?: string;
-  prop_130?: string;
-  prop_487?: string;
-  prop_860?: string;
   // PROPS_PLACEHOLDER
 }
 
