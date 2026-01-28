4a6dff46788fe5bc7b67d21979b3ab9a6986169b
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:36:09 +0530

    feat(ui): create new UserDataRow module

--- src/generated/ui/UserDataRow.tsx
+++ src/generated/ui/UserDataRow.tsx
@@ -5,9 +5,6 @@ interface UserDataRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_562?: string;
-  prop_288?: string;
-  prop_494?: string;
   // PROPS_PLACEHOLDER
 }
 
