d22bfc1c1fe8bd276dd402eabdee6095f5057c41
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:14:03 +0530

    feat(ui): create new TableViewHeader module

--- src/generated/ui/TableViewHeader.tsx
+++ src/generated/ui/TableViewHeader.tsx
@@ -5,9 +5,6 @@ interface TableViewHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_831?: string;
-  prop_370?: string;
-  prop_949?: string;
   // PROPS_PLACEHOLDER
 }
 
