c0852cdaedc97e6704e2e895fe380188c60b3442
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:11:18 +0530

    feat(ui): create new SettingsListGroup module

--- src/generated/ui/SettingsListGroup.tsx
+++ src/generated/ui/SettingsListGroup.tsx
@@ -5,9 +5,6 @@ interface SettingsListGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_125?: string;
-  prop_366?: string;
-  prop_304?: string;
   // PROPS_PLACEHOLDER
 }
 
