e24f88c9426c9b5f328409cb170388cec8c1f494
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:15:52 +0530

    feat(ui): create new ModalLinkPanel module

--- src/generated/ui/ModalLinkPanel.tsx
+++ src/generated/ui/ModalLinkPanel.tsx
@@ -5,9 +5,6 @@ interface ModalLinkPanelProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_650?: string;
-  prop_265?: string;
-  prop_366?: string;
   // PROPS_PLACEHOLDER
 }
 
