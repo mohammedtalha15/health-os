43718902c55075164f8b28cee43c521f36a06f37
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:23:26 +0530

    feat(ui): create new ButtonWrapperGroup module

--- src/generated/ui/ButtonWrapperGroup.tsx
+++ src/generated/ui/ButtonWrapperGroup.tsx
@@ -5,9 +5,6 @@ interface ButtonWrapperGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_857?: string;
-  prop_435?: string;
-  prop_797?: string;
   // PROPS_PLACEHOLDER
 }
 
