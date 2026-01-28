fcb4fd8be0898d71c035077b3d12d303c5cc3a75
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:34:05 +0530

    feat(ui): create new AuthContainerbox module

--- src/generated/ui/AuthContainerbox.tsx
+++ src/generated/ui/AuthContainerbox.tsx
@@ -5,11 +5,6 @@ interface AuthContainerboxProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_489?: string;
-  prop_372?: string;
-  prop_773?: string;
-  prop_527?: string;
-  prop_121?: string;
   // PROPS_PLACEHOLDER
 }
 
