c79e41dc83b57a71070e93e8f7368acceb3e6252
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:56:53 +0530

    feat(ui): create new DashboardViewGroup module

--- src/generated/ui/DashboardViewGroup.tsx
+++ src/generated/ui/DashboardViewGroup.tsx
@@ -5,9 +5,6 @@ interface DashboardViewGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_203?: string;
-  prop_326?: string;
-  prop_408?: string;
   // PROPS_PLACEHOLDER
 }
 
