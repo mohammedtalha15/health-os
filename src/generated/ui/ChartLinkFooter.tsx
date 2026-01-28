bb1c4309edb7b7fdd365dc987fac4041d2c255e3
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:04:21 +0530

    feat(ui): create new ChartLinkFooter module

--- src/generated/ui/ChartLinkFooter.tsx
+++ src/generated/ui/ChartLinkFooter.tsx
@@ -5,9 +5,6 @@ interface ChartLinkFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_242?: string;
-  prop_380?: string;
-  prop_982?: string;
   // PROPS_PLACEHOLDER
 }
 
