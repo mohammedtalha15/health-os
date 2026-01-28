d1e6189be59230a504b43ec5b911bbf9ced19ed7
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:45:27 +0530

    feat(ui): create new AuthSelectRow module

--- src/generated/ui/AuthSelectRow.tsx
+++ src/generated/ui/AuthSelectRow.tsx
@@ -5,9 +5,6 @@ interface AuthSelectRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_269?: string;
-  prop_372?: string;
-  prop_677?: string;
   // PROPS_PLACEHOLDER
 }
 
