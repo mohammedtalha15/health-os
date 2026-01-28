10ad2f37c6013aa19fba53a96ad4745bc073e659
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:15:17 +0530

    feat(ui): create new AdminLinkFooter module

--- src/generated/ui/AdminLinkFooter.tsx
+++ src/generated/ui/AdminLinkFooter.tsx
@@ -5,10 +5,6 @@ interface AdminLinkFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_964?: string;
-  prop_246?: string;
-  prop_161?: string;
-  prop_456?: string;
   // PROPS_PLACEHOLDER
 }
 
