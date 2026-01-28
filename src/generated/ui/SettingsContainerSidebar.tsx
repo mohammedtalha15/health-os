d65341a0b1004897633130bdf7bbb23067f1e270
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:17:38 +0530

    feat(ui): create new SettingsContainerSidebar module

--- src/generated/ui/SettingsContainerSidebar.tsx
+++ src/generated/ui/SettingsContainerSidebar.tsx
@@ -5,9 +5,6 @@ interface SettingsContainerSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_801?: string;
-  prop_785?: string;
-  prop_794?: string;
   // PROPS_PLACEHOLDER
 }
 
