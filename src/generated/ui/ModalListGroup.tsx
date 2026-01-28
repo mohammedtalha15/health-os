fad461c09e7f0a2e0f4a2307d61ffe131abf4701
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 00:55:25 +0530

    feat(ui): create new ModalListGroup module

--- src/generated/ui/ModalListGroup.tsx
+++ src/generated/ui/ModalListGroup.tsx
@@ -5,9 +5,6 @@ interface ModalListGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_152?: string;
-  prop_764?: string;
-  prop_128?: string;
   // PROPS_PLACEHOLDER
 }
 
