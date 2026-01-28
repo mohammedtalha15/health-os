4057303d5feae574c030bc61c5a74ddde53344bb
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:08:47 +0530

    feat(ui): create new ProfileInputHeader module

--- src/generated/ui/ProfileInputHeader.tsx
+++ src/generated/ui/ProfileInputHeader.tsx
@@ -5,9 +5,6 @@ interface ProfileInputHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_405?: string;
-  prop_333?: string;
-  prop_812?: string;
   // PROPS_PLACEHOLDER
 }
 
