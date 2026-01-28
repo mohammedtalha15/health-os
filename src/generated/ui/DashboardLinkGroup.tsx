1c752792637558a62cf88e98a158394a84119ba3
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:48:08 +0530

    feat(ui): create new DashboardLinkGroup module

--- src/generated/ui/DashboardLinkGroup.tsx
+++ src/generated/ui/DashboardLinkGroup.tsx
@@ -5,9 +5,6 @@ interface DashboardLinkGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_957?: string;
-  prop_119?: string;
-  prop_188?: string;
   // PROPS_PLACEHOLDER
 }
 
