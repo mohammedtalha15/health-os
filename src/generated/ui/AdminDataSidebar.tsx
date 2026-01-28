a496408af55618af88cea33078dc76e384305a8b
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:46:08 +0530

    feat(ui): create new AdminDataSidebar module

--- src/generated/ui/AdminDataSidebar.tsx
+++ src/generated/ui/AdminDataSidebar.tsx
@@ -5,9 +5,6 @@ interface AdminDataSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_770?: string;
-  prop_858?: string;
-  prop_918?: string;
   // PROPS_PLACEHOLDER
 }
 
