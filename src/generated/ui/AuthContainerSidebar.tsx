0fd906cdfb47da1e583326eac8252272891648ad
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:36:53 +0530

    feat(ui): create new AuthContainerSidebar module

--- src/generated/ui/AuthContainerSidebar.tsx
+++ src/generated/ui/AuthContainerSidebar.tsx
@@ -5,9 +5,6 @@ interface AuthContainerSidebarProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_726?: string;
-  prop_902?: string;
-  prop_781?: string;
   // PROPS_PLACEHOLDER
 }
 
