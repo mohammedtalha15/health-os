24b56f1fc17efdd983d98f305aab8b706e765e6f
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:24:10 +0530

    feat(ui): create new ChartListPanel module

--- src/generated/ui/ChartListPanel.tsx
+++ src/generated/ui/ChartListPanel.tsx
@@ -5,9 +5,6 @@ interface ChartListPanelProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_358?: string;
-  prop_601?: string;
-  prop_136?: string;
   // PROPS_PLACEHOLDER
 }
 
