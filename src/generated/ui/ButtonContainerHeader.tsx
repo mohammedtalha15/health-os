848c41794bcefaa74ebadf04ca548210817e2dc8
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:39:51 +0530

    feat(ui): create new ButtonContainerHeader module

--- src/generated/ui/ButtonContainerHeader.tsx
+++ src/generated/ui/ButtonContainerHeader.tsx
@@ -5,9 +5,6 @@ interface ButtonContainerHeaderProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_108?: string;
-  prop_600?: string;
-  prop_535?: string;
   // PROPS_PLACEHOLDER
 }
 
