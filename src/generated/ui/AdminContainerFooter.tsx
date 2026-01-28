e7daa470a7470e5d27555f305874c302a804ae04
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:41:15 +0530

    feat(ui): create new AdminContainerFooter module

--- src/generated/ui/AdminContainerFooter.tsx
+++ src/generated/ui/AdminContainerFooter.tsx
@@ -5,19 +5,6 @@ interface AdminContainerFooterProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_551?: string;
-  prop_223?: string;
-  prop_698?: string;
-  prop_480?: string;
-  prop_306?: string;
-  prop_835?: string;
-  prop_825?: string;
-  prop_650?: string;
-  prop_772?: string;
-  prop_988?: string;
-  prop_405?: string;
-  prop_169?: string;
-  prop_572?: string;
   // PROPS_PLACEHOLDER
 }
 
