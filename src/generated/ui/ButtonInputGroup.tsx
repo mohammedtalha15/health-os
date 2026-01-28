12dca9a48a57859ed1e4ca5be4676e92731d50f2
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:24:36 +0530

    feat(ui): create new ButtonInputGroup module

--- src/generated/ui/ButtonInputGroup.tsx
+++ src/generated/ui/ButtonInputGroup.tsx
@@ -5,9 +5,6 @@ interface ButtonInputGroupProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_976?: string;
-  prop_919?: string;
-  prop_983?: string;
   // PROPS_PLACEHOLDER
 }
 
