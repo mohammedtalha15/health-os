24a19e8d794870fe71ee53fcadf6c39313ef39d0
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 02:10:27 +0530

    feat(ui): create new ButtonToggleRow module

--- src/generated/ui/ButtonToggleRow.tsx
+++ src/generated/ui/ButtonToggleRow.tsx
@@ -5,9 +5,6 @@ interface ButtonToggleRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_186?: string;
-  prop_835?: string;
-  prop_260?: string;
   // PROPS_PLACEHOLDER
 }
 
