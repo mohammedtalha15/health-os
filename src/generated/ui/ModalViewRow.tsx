6f394dcf6dae1d7181347e23947b16352e1bdc69
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-01-29 01:59:27 +0530

    feat(ui): create new ModalViewRow module

--- src/generated/ui/ModalViewRow.tsx
+++ src/generated/ui/ModalViewRow.tsx
@@ -5,26 +5,6 @@ interface ModalViewRowProps {
   id?: string;
   className?: string;
   children?: React.ReactNode;
-  prop_415?: string;
-  prop_826?: string;
-  prop_300?: string;
-  prop_595?: string;
-  prop_674?: string;
-  prop_215?: string;
-  prop_604?: string;
-  prop_558?: string;
-  prop_868?: string;
-  prop_553?: string;
-  prop_896?: string;
-  prop_187?: string;
-  prop_162?: string;
-  prop_791?: string;
-  prop_544?: string;
-  prop_900?: string;
-  prop_518?: string;
-  prop_423?: string;
-  prop_428?: string;
-  prop_138?: string;
   // PROPS_PLACEHOLDER
 }
 
